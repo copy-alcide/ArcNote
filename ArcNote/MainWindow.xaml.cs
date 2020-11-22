95bf18bc5fd0f64986e26251eee2720198c8d172
Copy_Alcide <67416918+copy-alcide@users.noreply.github.com>
2020-11-22 14:33:02 +0800

    Add files via upload

---
+++ ArcNote/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.IO;
+
+
+namespace ArcNote
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Topmost = true;
+        }
+        private void Move_MouseMove(object sender, MouseEventArgs e) //移動窗口
+
+        {
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+
+            {
+
+                this.DragMove();
+
+            }
+
+        }
+        bool boldstate = false;//加粗狀態
+        private void InputRichTextBox_TextBoldEnable(object sender, EventArgs e)//啟用加粗
+        {
+            
+            InputRichTextBox.FontWeight = FontWeights.Bold;
+            boldstate = true;
+        }
+        private void InputRichTextBox_TextBoldDisable(object sender, EventArgs e)//禁用加粗
+        {
+
+            InputRichTextBox.FontWeight = FontWeights.Normal;
+            boldstate = false;
+        }
+        bool italicstate = false;
+        private void InputRichTextBox_TextItalicEnable(object sender, EventArgs e)//啓用意大利斜體
+        {
+            InputRichTextBox.FontStyle = FontStyles.Italic;
+            italicstate = true;
+        }
+        private void InputRichTextBox_TextItalicDisable(object sender, EventArgs e)//禁用意大利斜體
+        {
+            InputRichTextBox.FontStyle = FontStyles.Normal;
+            italicstate = false;
+        }
+        private void InputRichTextBox_OpenFile(object sender, RoutedEventArgs e) //打開檔案
+        {
+            string folderName = @"%Appdata%";
+            string path = System.IO.Path.Combine(folderName, "ArcNote");
+            string fileName = "Note.xml";
+            path = Environment.ExpandEnvironmentVariables(path);
+            System.IO.Directory.CreateDirectory(path);
+            path = System.IO.Path.Combine(path, fileName);
+            if (System.IO.File.Exists(path))
+            {
+                TextRange range;
+                FileStream File;
+                range = new TextRange(InputRichTextBox.Document.ContentStart, InputRichTextBox.Document.ContentEnd);
+                File = new FileStream(path, FileMode.Open);
+                range.Load(File, DataFormats.Xaml) ;
+                File.Close();
+            }
+            else
+            {
+                TextRange range;
+                FileStream file;
+                range = new TextRange(InputRichTextBox.Document.ContentStart, InputRichTextBox.Document.ContentStart);
+                file = new FileStream(path, FileMode.Create);
+                range.Save(file, DataFormats.Xaml);
+                MessageBox.Show("無記事檔案，現已建立佔位空檔案", "", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        private void InputRichTextBox_SaveFile(object sender, RoutedEventArgs e) //存爲檔案
+        {
+            if (boldstate == true)//臨時去加粗
+            {
+                InputRichTextBox.Document.FontWeight = FontWeights.Normal;
+            }
+            if (italicstate == true)//臨時去意大利體
+            {
+                InputRichTextBox.Document.FontStyle = FontStyles.Normal;
+            }
+            string folderName = @"%Appdata%";
+            string path = System.IO.Path.Combine(folderName, "ArcNote");
+            string fileName = "Note.xml";
+            path = Environment.ExpandEnvironmentVariables(path);
+            System.IO.Directory.CreateDirectory(path);
+            path = System.IO.Path.Combine(path, fileName);
+            TextRange range;
+            FileStream file;
+            range = new TextRange(InputRichTextBox.Document.ContentStart, InputRichTextBox.Document.ContentEnd);
+            file = new FileStream(path, FileMode.Create);
+            range.Save(file, DataFormats.Xaml);
+            file.Close();
+            if (boldstate == true)//恢復加粗
+            {
+                InputRichTextBox.Document.FontWeight = FontWeights.Bold;
+            }
+            if (italicstate == true)//恢復意大利體
+            {
+                InputRichTextBox.Document.FontStyle = FontStyles.Italic;
+            }
+            MessageBox.Show("檔案已儲存", "", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        private void InputRichTextBox_DeleteFile(object sender, RoutedEventArgs e)
+        {
+            string folderName = @"%Appdata%";
+            string path = System.IO.Path.Combine(folderName, "ArcNote");
+            string fileName = "Note.xml";
+            path = Environment.ExpandEnvironmentVariables(path);
+            path = System.IO.Path.Combine(path, fileName);
+            System.IO.File.Delete(path);
+            path = System.IO.Path.Combine(folderName, "ArcNote");
+            path = Environment.ExpandEnvironmentVariables(path);
+            System.IO.Directory.Delete(path);
+            MessageBox.Show("檔案已刪除", "", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        private void Exit(object sender, RoutedEventArgs e) //退出
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# ArcNote
-A simple note application for every Windows user.
+# ArcNote_ver_2.0
+
+## 簡介
+
+<img src="/Images/Icon.ico" style="zoom:30%;" />
+
+由個人需求產生出的一款便籤程式，在莫名其妙的推動力驅使下，於最初簡陋應用的基礎上建構起了一個還算堪用的軟體。
+
+## 功能
+
+- 寫便籤
+
+- 讀寫數據（目前僅支援讀寫單個檔案）
+
+- 字體格式（目前僅支援加粗和應用意大利體）
+## 注意
+
+- 本程式數據存於您系統上的 "%Appdata%\ArcNote" 目錄下，使用本程式內建的「刪除檔案」功能即可刪除所有存於本機上的數據檔案。
+
+- 目前本程式僅支援給所有文本同時應用字體格式。
+
+- 目前本程式在儲存檔案時不支援儲存字體格式。
+
+## 未來可能更新
+
+- 添加多檔案讀寫功能
+- 添加儲存字體格式功能
+- 允許用戶自選字體
+## 特別提及
+
+- 文件輸入輸出功能部分代碼來自 Microsoft Docs
+
+  [RichTextBox Overview - WPF .NET Framework | Microsoft Docs](https://docs.microsoft.com/en-us/dotnet/desktop/wpf/controls/richtextbox-overview?view=netframeworkdesktop-4.8) 
+
+- 圖標繪師：一般通过白泽球
+
+  QQ：1686448499
+
